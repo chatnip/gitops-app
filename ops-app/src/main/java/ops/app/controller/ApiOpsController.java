0e5e4d925842f684830778851ebe2eec7a8e2aeb
chatnip <commcencer@gmail.com>
2026-05-19 09:11:17 +0900

    Update ApiOpsController.java

--- ops-app/src/main/java/ops/app/controller/ApiOpsController.java
+++ ops-app/src/main/java/ops/app/controller/ApiOpsController.java
@@ -4,18 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api")
 public class ApiOpsController {
-
-    @GetMapping("/ops")
-    public String OpsTest() {
-        return "Get - /api/ops";
-    }
-    
-    @GetMapping("/version")
-    public String version() {
-        return "v1.0.0";
-    }
+	
+	@GetMapping("/ops")
+	public String OpsTest() {
+		return "Get - /api/ops";
+	}
+	
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "OK\n";
+	}
+	
+	@GetMapping("/feat-ops")
+	public String opsCheck() {
+		return "Get - /api/feat-ops";
+	}
 }
